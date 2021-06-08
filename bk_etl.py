8b924647a3143691977baa08f7040164e988e6ec
ds <fp.ob>
2021-06-08 12:06:13 +0200

    log pandas

--- bk_etl.py
+++ bk_etl.py
@@ -26,56 +26,63 @@ files= glob2.glob('/datadrive/**/booking16*.csv')
 
 
 def etl_pipe(file):
-    cnxn = pyodbc.connect('DRIVER='+driver+';SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
-    cursor = cnxn.cursor()
-    df = pd.read_csv(file, sep = '\t')
-    df['PAYS'] = df.apply(lambda x: x['url'].split('/')[4].upper(), axis=1)
 
-    for a in range(len(df)):
-        time.sleep(random.randint(5,15)/10)
-        url=str(df.iloc[a]['url'])
-        url="'"+url.replace("'",'')+"'"
-        name=str(df.iloc[a]['name'])
-        name=name.replace("'",'')
-        description=str(df.iloc[a]['description'])
-        description=description.replace("'",'')
-        review=str(df.iloc[a]['review'])
-        review="'"+review.replace("'",'')+"'"
-        score="'"+str(df.iloc[a]['score'])+"'"
-        num_reviews="'"+str(df.iloc[a]['number of reviews'])+"'"
-        type_=str(df.iloc[a]['type of property'])
-        type_=type_.replace("'",'')
-        adrs=str(df.iloc[a]['address'])
-        adrs=adrs.replace("'",'')
-        star=str(df.iloc[a]['stars'])
-        recommendation=str(df.iloc[a]['recommend'])
-        recommendation=recommendation.replace("'",'')
-        descdetail=str(df.iloc[a]['descdetail'])
-        descdetail=descdetail.replace("'",'')
-        equip=str(df.iloc[a]['equip'])
-        equip=equip.replace("'",'')
-        equipdetail=str(df.iloc[a]['equipdetail'])
-        equipdetail=equipdetail.replace("'",'')
-        lat="'"+str(df.iloc[a]['lat'])+"'"
-        long="'"+str(df.iloc[a]['long'])+"'"
-        hotelchain=str(df.iloc[a]['hotelchain'])
-        hotelchain="'"+hotelchain.replace("'",'')+"'"
-        restaurant=str(df.iloc[a]['restaurant'])
-        restaurant=restaurant.replace("'",'')
-        poi=str(df.iloc[a]['POIs'])
-        poi=poi.replace("'",'')
-        comment=str(df.iloc[a]['Comments'])
-        comment=comment.replace("'",'')
-        pays=str(df.iloc[a]['PAYS'])
-        pays=pays.replace("'",'')
-        query="if (select count(*) from dbo.Booking2021 WHERE URL_=%s)=0 insert into dbo.Booking2021(URL_,NAME_,DESCRIPTION_,REVIEW_,SCORE_,NUMREVIEW_,TYPE_,ADDRESSE_,STARS_,DESCDETAIL_,EQUIP_,EQUIPDETAIL_,LAT,LONG,HOTELCHAIN,RESTAURANT,POIS,COMMENTS,PAYS,RECOMMEND_) values(%s,'%s','%s',%s,%s,%s,'%s','%s','%s','%s','%s','%s',%s,%s,%s,'%s','%s','%s','%s','%s')" % (url,url,name,description,review,score,num_reviews,type_,adrs,star,descdetail,equip,equipdetail,lat,long,hotelchain,restaurant,poi,comment,pays,recommendation)
-        try:
-            cursor.execute(query)
-            cnxn.commit()
-        except Exception as e :
-            with open('logsql.txt','a') as flog:
-                print(e, file=flog)
-                print(query, file=flog)
+    try:
+        cnxn = pyodbc.connect('DRIVER='+driver+';SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
+        cursor = cnxn.cursor()
+        df = pd.read_csv(file, sep = '\t')
+        df['PAYS'] = df.apply(lambda x: x['url'].split('/')[4].upper(), axis=1)
+
+        for a in range(len(df)):
+            time.sleep(random.randint(5,15)/10)
+            url=str(df.iloc[a]['url'])
+            url="'"+url.replace("'",'')+"'"
+            name=str(df.iloc[a]['name'])
+            name=name.replace("'",'')
+            description=str(df.iloc[a]['description'])
+            description=description.replace("'",'')
+            review=str(df.iloc[a]['review'])
+            review="'"+review.replace("'",'')+"'"
+            score="'"+str(df.iloc[a]['score'])+"'"
+            num_reviews="'"+str(df.iloc[a]['number of reviews'])+"'"
+            type_=str(df.iloc[a]['type of property'])
+            type_=type_.replace("'",'')
+            adrs=str(df.iloc[a]['address'])
+            adrs=adrs.replace("'",'')
+            star=str(df.iloc[a]['stars'])
+            recommendation=str(df.iloc[a]['recommend'])
+            recommendation=recommendation.replace("'",'')
+            descdetail=str(df.iloc[a]['descdetail'])
+            descdetail=descdetail.replace("'",'')
+            equip=str(df.iloc[a]['equip'])
+            equip=equip.replace("'",'')
+            equipdetail=str(df.iloc[a]['equipdetail'])
+            equipdetail=equipdetail.replace("'",'')
+            lat="'"+str(df.iloc[a]['lat'])+"'"
+            long="'"+str(df.iloc[a]['long'])+"'"
+            hotelchain=str(df.iloc[a]['hotelchain'])
+            hotelchain="'"+hotelchain.replace("'",'')+"'"
+            restaurant=str(df.iloc[a]['restaurant'])
+            restaurant=restaurant.replace("'",'')
+            poi=str(df.iloc[a]['POIs'])
+            poi=poi.replace("'",'')
+            comment=str(df.iloc[a]['Comments'])
+            comment=comment.replace("'",'')
+            pays=str(df.iloc[a]['PAYS'])
+            pays=pays.replace("'",'')
+            query="if (select count(*) from dbo.Booking2021 WHERE URL_=%s)=0 insert into dbo.Booking2021(URL_,NAME_,DESCRIPTION_,REVIEW_,SCORE_,NUMREVIEW_,TYPE_,ADDRESSE_,STARS_,DESCDETAIL_,EQUIP_,EQUIPDETAIL_,LAT,LONG,HOTELCHAIN,RESTAURANT,POIS,COMMENTS,PAYS,RECOMMEND_) values(%s,'%s','%s',%s,%s,%s,'%s','%s','%s','%s','%s','%s',%s,%s,%s,'%s','%s','%s','%s','%s')" % (url,url,name,description,review,score,num_reviews,type_,adrs,star,descdetail,equip,equipdetail,lat,long,hotelchain,restaurant,poi,comment,pays,recommendation)
+            try:
+                cursor.execute(query)
+                cnxn.commit()
+            except Exception as e :
+                with open('logsql.txt','a') as flog:
+                    print(e, file=flog)
+                    print(query, file=flog)
+    except Exception as e:
+        with open('logpandas.txt','a') as flog:
+            print(e, file=flog)
+            print(query, file=flog)
+
 
 with concurrent.futures.ProcessPoolExecutor(max_workers=3) as executor:
     	future_to_url = {executor.submit(etl_pipe, file): file for file in files}
