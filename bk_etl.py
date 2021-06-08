40c0ab608feb3bf685f335d1d08acc388a420f1d
ds <fp.ob>
2021-06-08 12:42:14 +0200

    bug 1

--- bk_etl.py
+++ bk_etl.py
@@ -32,7 +32,8 @@ def fillcountry(x):
 
 
 def etl_pipe(file):
-
+    
+    df = pd.read_csv(file, sep = '\t')
     with open('logio.txt','a') as flog:
         length = str(len(df))
         message = 'Processing df ' + file + ' of length: '+length
